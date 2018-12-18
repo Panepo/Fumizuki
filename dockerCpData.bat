70e9eb33076e6ae31d511310385007b808346007
Panepo <whisper.ko@gmail.com>
2018-12-18 14:07:05 +0800

    update
    update

--- dockerCpData.bat
+++ dockerCpData.bat
@@ -5,4 +5,4 @@ docker cp ./checkpoint %1:/home/jovyan/work
 docker cp ./image %1:/home/jovyan/work
 docker cp ./data %1:/home/jovyan/work
 
-docker cp ./utility %1:/home/jovyan/work
+docker cp ./util %1:/home/jovyan/work
